17916317d46ef8a230b6dfa07fc4a06b32f33709
DEVMANISHOFFl <devmanishofficial@gmail.com>
2024-12-16 15:07:14 +0530

    Fix

--- client/src/features/api/courseProgressApi.js
+++ client/src/features/api/courseProgressApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_PROGRESS_API = process.env.REACT_APP_API_URL || "http://localhost:8080/api/v1/progress";
+const COURSE_PROGRESS_API =  "https://course-selling-website-blue.vercel.app/api/v1/progress";
 
 export const courseProgressApi = createApi({
   reducerPath: "courseProgressApi",
--- client/src/features/api/purchaseApi.js
+++ client/src/features/api/purchaseApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_PURCHASE_API =process.env.REACT_APP_API_URL ||  "http://localhost:8080/api/v1/purchase";
+const COURSE_PURCHASE_API =  "https://course-selling-website-blue.vercel.app/api/v1/purchase";
 
 export const purchaseApi = createApi({
   reducerPath: "purchaseApi",

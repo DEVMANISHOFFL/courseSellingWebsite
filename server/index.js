4ebcbdcc32e4c7f16302db00078cbb427e7566b7
DEVMANISHOFFl <devmanishofficial@gmail.com>
2024-12-16 13:57:19 +0530

    adhad
    qa
    qa!
    !
    qa!
    
    lkasd
    lkjkjjla

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:"http://localhost:5174",
+    origin:"https://course-selling-website-blue.vercel.app",
     credentials:true
 }));
  

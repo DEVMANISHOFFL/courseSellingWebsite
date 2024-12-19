cd394bb525b3f4391f4c0bf2851aa20ea60f036d
DEVMANISHOFFl <devmanishofficial@gmail.com>
2024-12-19 13:44:29 +0530

    Fix

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ app.use("/api/v1/purchase", purchaseRoute);
 app.use("/api/v1/progress", courseProgressRoute);
  
  
-// app.listen(PORT, () => {
-//     console.log(`Server listen at port ${PORT}`);
-// })
+app.listen(PORT, () => {
+    console.log(`Server listen at port ${PORT}`);
+})
 
